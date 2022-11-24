425fa9e8e90abaac40c86e709955c1137b6178cf
Ben Wright <benwright9400@gmail.com>
2022-11-24 20:13:55 +0000

    add functions to interfacer file of business layer

---
+++ system/BusinessLogic/Interfacer.js
@@ -0,0 +1,72 @@
+const database = require("./../Data/Data");
+
+//elderly person
+async function addMedicalRequest() {
+
+}
+
+module.exports.addMedicalRequest = addMedicalRequest;
+
+async function addMedicalRequestDetails() {
+
+}
+
+module.exports.addMedicalRequestDetails = addMedicalRequestDetails;
+
+async function addSubjectAccessRequest() {
+
+}
+
+module.exports.addSubjectAccessRequest = addSubjectAccessRequest;
+
+async function acceptTCs() {
+
+}
+
+module.exports.acceptTCs = acceptTCs;
+
+async function stopTCs() {
+    
+}
+
+module.exports.stopTCs = stopTCs;
+
+//TPS decision maker
+async function getAnonymisedRegionalData() {
+
+}
+
+module.exports.getAnonymisedRegionalData = getAnonymisedRegionalData;
+
+//TPS manager
+async function getRegionalData() {
+
+}
+
+module.exports.getRegionalData = getRegionalData;
+
+async function assignCaseToStaff() {
+
+}
+
+module.exports.assignCaseToStaff = assignCaseToStaff;
+
+async function getStaff() {
+
+}
+
+module.exports.getStaff = getStaff;
+
+//TPS Staff
+async function getStaffCases() {
+
+}
+
+module.exports.getStaffCases = getStaffCases;
+
+//System adminsitrator
+async function createOrganisation() {
+
+}
+
+module.exports.createOrganisation = createOrganisation;
