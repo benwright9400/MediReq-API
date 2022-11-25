9e1652ea69c4af78acffaecbe06a62dbd0563560
Ben Wright <benwright9400@gmail.com>
2022-11-25 23:29:14 +0000

    add values file

---
+++ system/values.js
@@ -0,0 +1,10 @@
+//convert to procfile
+
+const mongooseConnection = "mongodb+srv://server:D0Vlk9mIbwI11IQt@medireq0.m7es4pc.mongodb.net/?retryWrites=true&w=majority";
+
+exports.mongooseConnection = mongooseConnection;
+
+const secretKey = "pp%i/3J6wbY(#rd_";
+
+exports.key = secretKey;
+
