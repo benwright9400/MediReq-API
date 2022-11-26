e50bab91b7be77a8995455fdfb2b35a4cfcb2a3a
Ben Wright <benwright9400@gmail.com>
2022-11-26 00:42:03 +0000

    add string conversion

--- system/Service/serviceInterface.js
+++ system/Service/serviceInterface.js
@@ -158,7 +158,7 @@ app.get("/TPS/staffCases", (req, res) => {
 async function checkSecurity(req, onSuccess, onFailure) {
 
     // let reqBody = await decryptReqBody(req, onFailure);
-    let reqBody = JSON.parse(req.body);
+    let reqBody = JSON.parse(toString(req.body));
     console.log(reqBody);
 
     if(reqBody === false) {
