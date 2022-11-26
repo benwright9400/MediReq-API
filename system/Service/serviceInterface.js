22eea355b41071c1a95854df9d8dc79d3e1581e3
Ben Wright <benwright9400@gmail.com>
2022-11-26 01:02:23 +0000

    set urlencoded true

--- system/Service/serviceInterface.js
+++ system/Service/serviceInterface.js
@@ -8,7 +8,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
 
